9d1da27c40b5d7416e43c448b03423a978557ffb
Ahmed Zobaer <zobayer.me@gmail.com>
2022-04-02 23:54:09 +0600

    login api created

--- api_backend/routes/auth.js
+++ api_backend/routes/auth.js
@@ -37,16 +37,38 @@ router.post('/registration', async (request, response) => {
 //user Login
 router.post('/login', async (request, response) => {
     const input = request.body;
-    Users.findOne({username: input.username}, function(error, data) {
-        if(error){
-            response.status(200).json({status: 500, message: error.message});
-        }else{
-            bcrypt.compare(input.password, data.password, function(err, check) {
+    let user = await Users.findOne({username: input.username}).select("+password");
+    if(user != null){
+        bcrypt.compare(input.password, user.password, async (err, check) => {
+            if(err){
+                response.status(200).json({status: 500, message: err.message});
+            }else{
+                if(check === true){
+                    let token = randomToken(64);
+                    await Users.findOneAndUpdate({username: input.username}, {token: token});
+                    response.status(200).json({status: 200, message: 'Login successful'});
+                }else{
+                    response.status(200).json({status: 500, message: 'Invalid Credentials'});
+                }
+            }
+        });
+    }else{
+        response.status(200).json({status: 500, message: 'Invalid Credentials'});
+    }
 
-            });
-            response.status(200).json({status: 200, data: check});
-        }
-    }).select("+password");
+
+    // Users.findOne({username: input.username}, function(error, data) {
+    //     if(error){
+    //         response.status(200).json({status: 500, message: error.message});
+    //     }else{
+    //         bcrypt.compare(input.password, data.password, function(err, check) {
+    //
+    //         });
+    //         response.status(200).json({status: 200, data: check});
+    //     }
+    // }).select("+password");
+
+
 })
 
 
