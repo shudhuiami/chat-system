57d91369a80cdf39067810a0229fd738e97400db
Ahmed Zobaer <zobayer.me@gmail.com>
2022-04-03 17:11:39 +0600

    cros implemented

--- api_backend/controllers/AuthController.js
+++ api_backend/controllers/AuthController.js
@@ -1,50 +1,47 @@
-const Users = require('../models/users');
-const bcrypt = require('bcrypt');
-const randomToken = require('random-token');
+import Users from '../models/users.js';
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
+import { nanoid } from "nanoid";
 
 
-const Login = async function (){
-    const input = request.body;
-    let user = await Users.findOne({username: input.username}).select("+password");
-    if(user != null){
-        bcrypt.compare(input.password, user.password, async (err, check) => {
-            if(err){
-                response.status(200).json({status: 500, message: err.message});
-            }else{
-                if(check === true){
-                    let token = randomToken(64);
-                    await Users.findOneAndUpdate({username: input.username}, {token: token});
-                    response.status(200).json({status: 200, message: 'Login successful'});
+const AuthController = {
+    Registration: async (request, response) => {
+        try {
+            const input = request.body;
+            let HashPassword = await bcrypt.hash(input.password, saltRounds);
+            const user = new Users({
+                name: input.name,
+                email: input.email,
+                username: input.username,
+                password: HashPassword
+            })
+            await user.save();
+            response.status(200).json({status: 200, msg: 'New user created successfully'})
+        }catch (e) {
+            response.status(200).json({status: 500, message: e.errors});
+        }
+    },
+    Login: async (request, response) => {
+        const input = request.body;
+        let user = await Users.findOne({username: input.username}).select("+password");
+        if(user != null){
+            bcrypt.compare(input.password, user.password, async (err, check) => {
+                if(err){
+                    response.status(200).json({status: 500, message: err.message});
                 }else{
-                    response.status(200).json({status: 500, message: 'Invalid Credentials'});
+                    if(check === true){
+                        let token = nanoid(64);
+                        await Users.findOneAndUpdate({username: input.username}, {token: token});
+                        response.status(200).json({status: 200, message: 'Login successful'});
+                    }else{
+                        response.status(200).json({status: 500, message: 'Invalid Credentials'});
+                    }
                 }
-            }
-        });
-    }else{
-        response.status(200).json({status: 500, message: 'Invalid Credentials'});
-    }
+            });
+        }else{
+            response.status(200).json({status: 500, message: 'Invalid Credentials'});
+        }
+    },
 }
 
-const Register = async function (){
-    const input = request.body;
-    let user = await Users.findOne({username: input.username}).select("+password");
-    if(user != null){
-        bcrypt.compare(input.password, user.password, async (err, check) => {
-            if(err){
-                response.status(200).json({status: 500, message: err.message});
-            }else{
-                if(check === true){
-                    let token = randomToken(64);
-                    await Users.findOneAndUpdate({username: input.username}, {token: token});
-                    response.status(200).json({status: 200, message: 'Login successful'});
-                }else{
-                    response.status(200).json({status: 500, message: 'Invalid Credentials'});
-                }
-            }
-        });
-    }else{
-        response.status(200).json({status: 500, message: 'Invalid Credentials'});
-    }
-}
-
-export {Login, Register};
+export default AuthController;
--- api_backend/routes/auth.js
+++ api_backend/routes/auth.js
@@ -1,50 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const Users = require('../models/users');
-const randomToken = require('random-token');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const AuthControllers = require('../controllers/AuthController')
-
-//Get All users
-router.get('/users', async (request, response) => {
-    try{
-        const users = await Users.find();
-        response.status(200).json({status: 200, data: users});
-    }catch (e) {
-        response.status(200).json({status: 500, message: e.errors});
-    }
-})
-
+import express from 'express'
+const AuthRoutes = express.Router();
+import AuthController from "../controllers/AuthController.js";
 
 //User Registration
-// router.post('/registration', async (request, response) => {
-//     AuthControllers.Login(request, response)
-// })
+AuthRoutes.post('/registration', async (request, response) => AuthController.Registration(request, response))
 
 //user Login
-// router.post('/login', async (request, response) => AuthControllers.Login(request, response))
+AuthRoutes.post('/login', async (request, response) => AuthController.Login(request, response))
 
-// async function Login(request, response) {
-//     const input = request.body;
-//     let user = await Users.findOne({username: input.username}).select("+password");
-//     if(user != null){
-//         bcrypt.compare(input.password, user.password, async (err, check) => {
-//             if(err){
-//                 response.status(200).json({status: 500, message: err.message});
-//             }else{
-//                 if(check === true){
-//                     let token = randomToken(64);
-//                     await Users.findOneAndUpdate({username: input.username}, {token: token});
-//                     response.status(200).json({status: 200, message: 'Login successful'});
-//                 }else{
-//                     response.status(200).json({status: 500, message: 'Invalid Credentials'});
-//                 }
-//             }
-//         });
-//     }else{
-//         response.status(200).json({status: 500, message: 'Invalid Credentials'});
-//     }
-// }
 
-module.exports = router;
+export default AuthRoutes;
